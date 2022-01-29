5ca2f9c1fdc62c3ec11af5db7586ac51df340ee0
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-01-30 00:25:38 +0530

    basic template preapared

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expence_planner/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        fontFamily: GoogleFonts.aBeeZee().fontFamily,
+        fontFamily: GoogleFonts.laila().fontFamily,
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
@@ -25,12 +26,63 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Transaction> transactions = [
+      Transaction(
+        id: 't1',
+        title: 'New Shoes',
+        amount: 400,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't2',
+        title: 'New Socks',
+        amount: 100,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't3',
+        title: 'Weekly Groceries',
+        amount: 700,
+        date: DateTime.now(),
+      ),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter App'),
       ),
-      body: const Center(
-        child: Text('Widget Playground'),
+      body: Column(
+        children: <Widget>[
+          const Card(
+            child: Text(
+              'CHART',
+              textScaleFactor: 3.0,
+            ),
+          ),
+          Column(
+              children: transactions.map(((tx) {
+            return Card(
+              child: Row(
+                children: [
+                  Container(
+                    child: Text(
+                      tx.amount.toString(),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        tx.title,
+                      ),
+                      Text(
+                        tx.date.toString(),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            );
+          })).toList()),
+        ],
       ),
     );
   }
